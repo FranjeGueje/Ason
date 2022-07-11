0f7d3f369180b91dbae6fee121dabe1b9fd6b4a2
Paco Guerrero <fjgj1@hotmail.com>
2022-07-12 00:35:07 +0200

    Crear instalador

---
+++ INSTALL/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#
+##
+## Downloading files...
+##
+#
+
+cd ~ && git clone https://github.com/FranjeGueje/Ason.git && cd Ason && BUILD/build.sh
+
+exit 0
+
---
+++ nile.sh
@@ -0,0 +1,1 @@
+nile/bin/nile
